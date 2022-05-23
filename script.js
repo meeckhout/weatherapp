a59d845cc99f93ff27b1fb4ebda1472e8985de43
meeckhout <marthe.eeckhout@gmail.com>
2022-05-23 14:55:11 +0200

    Add api key

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-// import { api } from "config.js";
-const lat = '59.334591'
-const lon = '18.063240'
-const city = document.getElementById('city')
-let temp = document.getElementById('temperature')
+import { API_KEY } from './config.js'
+
+const city = document.getElementById('cityID')
+const button = document.querySelector('button')
+const temp = document.getElementById('temperature')
 const description = document.getElementById('description')
 const date = document.getElementById('date')
-const url = 'https://api.openweathermap.org/data/2.5/weather?id=lat=59.3293&lon=18.0686&appid=0aedbe0e2b61bf889a956fc2b9a1ca9d'
+const url = `https://api.openweathermap.org/data/2.5/weather?q=gent&appid=${API_KEY}`
 
+button.addEventListener('click', event => {
+    getWeather()
+})
+
+function getWeather() {
   fetch(url)
-  .then((response) => {
-    return response.json();
-  })
+  .then((response) => 
+  response.json())
   .then((data) => {
-    temperature = data;
+    console.log(data)
   })
-  .catch(function(error) {
-    console.log(error);
-  });
+  .catch(() => {
+    alert('Search for valid city')
+  }) 
+}
+
