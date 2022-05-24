7b9e0bbbd5c08dd4ba1f39ff15b774c34582b321
meeckhout <marthe.eeckhout@gmail.com>
2022-05-24 11:10:11 +0200

    Add button event

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 import { API_KEY } from './config.js'
-
+const input = document.getElementById('cityName')
 const city = document.getElementById('cityID')
 const button = document.querySelector('button')
 const temp = document.getElementById('temperature')
 const description = document.getElementById('description')
+const image = document.getElementById('image')
 const url = `https://api.openweathermap.org/data/2.5/weather?q=gent&units=metric&appid=${API_KEY}`
-
-
-// button.addEventListener('click', event => {
-//     getWeather()
-//     // showWeather()
-// })
+const btnUrl = `https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&units=metric&appid=${API_KEY}`
 
 window.onload = () => {
-    getWeather()
+  getWeather()
 }
+
+button.addEventListener('click', event => {
+    fetch(url)
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => {
+      const nameValue = data.name
+      const descriptionValue = data.weather['0']['description']
+      const temperatureValue = (data.main.temp).toFixed(0) + '°C'
+
+      city.innerHTML = `You're in ${nameValue}`
+      temp.innerHTML = `It is ${temperatureValue}`
+      description.innerHTML = `Feels a bit ${descriptionValue}`
+    })
+    .catch(err => 
+      alert('Enter valid city'))
+})
+
 
 function getWeather() {
   fetch(url)
@@ -22,10 +36,10 @@ function getWeather() {
   response.json())
   .then((data) => {
         city.innerHTML = data.name
-        temp.innerHTML = (data.main.temp).toFixed(0)
-        description.innerHTML = data.weather[0][main] 
+        temp.innerHTML = (data.main.temp).toFixed(0) + '°C'
+        description.innerHTML = data.weather[0]['main'] 
+        image.innerHTML = data.weather[0]['icon']
   })
   .catch(function() {
-    
   }) 
 }
