3664192c430efcbb29fe460240f5bffcd3e60965
meeckhout <marthe.eeckhout@gmail.com>
2022-05-24 11:52:28 +0200

    Add geolocation

--- script.js
+++ script.js
@@ -5,17 +5,28 @@ const button = document.querySelector('button')
 const temp = document.getElementById('temperature')
 const description = document.getElementById('description')
 const image = document.getElementById('image')
-const url = `https://api.openweathermap.org/data/2.5/weather?q=gent&units=metric&appid=${API_KEY}`
+let lat = []
+let long = []
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`
 const btnUrl = `https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&units=metric&appid=${API_KEY}`
+
+
+
+// const errorCallback = (error) => {console.error(error)}
+// const successCallback = (position) => {
+//     lat.push(position.coords.latitude)
+//     lon.push(position.coords.longitude)}
+
+// navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
 
 window.onload = () => {
   getWeather()
+  // weather()
 }
 
 button.addEventListener('click', event => {
     fetch(url)
     .then(response => response.json())
-    // .then(data => console.log(data))
     .then(data => {
       const nameValue = data.name
       const descriptionValue = data.weather['0']['description']
@@ -31,15 +42,23 @@ button.addEventListener('click', event => {
 
 
 function getWeather() {
-  fetch(url)
-  .then((response) => 
-  response.json())
-  .then((data) => {
-        city.innerHTML = data.name
-        temp.innerHTML = (data.main.temp).toFixed(0) + '°C'
-        description.innerHTML = data.weather[0]['main'] 
-        image.innerHTML = data.weather[0]['icon']
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition((position) =>{
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`
+
+    fetch(api)
+    .then((response) => 
+    response.json())
+    .then((data) => {
+          city.innerHTML = data.name
+          temp.innerHTML = (data.main.temp).toFixed(0) + '°C'
+          description.innerHTML = data.weather[0]['main'] 
+          image.innerHTML = data.weather[0]['icon']
+    })
+    .catch(function() {
+    }) 
   })
-  .catch(function() {
-  }) 
+}
 }
