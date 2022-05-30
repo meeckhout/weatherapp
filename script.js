77c4c0303189748f6bec3add3c05608189f8fa73
meeckhout <marthe.eeckhout@gmail.com>
2022-05-30 11:12:01 +0200

    Refactor js

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-import { API_KEY } from './config.js'
-
-/*TODO: ADD FIVE DAY WEATHER FORECAST*/
-const forecastTemp = document.querySelector('forecast-temp')
-const forecastDescription = document.querySelector('forecast-description')
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
+import {API_KEY} from './config.js'
 
 const input = document.getElementById('cityName')
 const button = document.querySelector('.fa-solid')
 const temp = document.getElementById('temperature')
 const description = document.getElementById('description')
-const image = document.getElementById('image')
 const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&units=metric&appid=${API_KEY}`
 
 let city = document.getElementById('cityID')
 let lat = []
 let lon = []
+let dayOfTheWeek
 
 window.onload = () => {
-  getWeather()
+    getWeather()
     document.getElementById('cityName').value = '';
 }
 
@@ -26,51 +20,52 @@ document.body.addEventListener('click', event => {
     document.getElementById('cityName').value = '';
 })
 
-function getWeather() {
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition((position) =>{
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
-
-    fetch(api)
-    .then((response) => 
-    response.json())
-    .then((data) => {
-          city.innerHTML = data.name
-          temp.innerHTML = (data.main.temp).toFixed(0) + '°C'
-          description.innerHTML = data.weather[0]['main'] 
-          image.innerHTML = data.weather[0]['icon']
-    })
-    .catch(function() {
-    }) 
-  })
-}}
-
 const inputCity = document.querySelector('.inputCity')
 inputCity.addEventListener('keypress', getLocationCoordinates)
 button.addEventListener('click', getLocationCoordinates)
 
-function getLocationCoordinates (event) {
-  if (event.keyCode == 13) {
-  getResults(inputCity.value)
-  } else {
-      getResults(inputCity.value)
-  }
+const getWeather = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+
+            fetch(api)
+                .then((response) =>
+                    response.json())
+                .then((data) => {
+                    city.innerHTML = data.name
+                    temp.innerHTML = (data.main.temp).toFixed(0) + '°C'
+                    description.innerHTML = data.weather[0]['main']
+                    image.innerHTML = data.weather[0]['icon']
+                })
+                .catch(function () {
+                })
+        })
+    }
 }
 
-function getResults (query) {
-  fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&units=metric&appid=${API_KEY}`)
-  .then((data) => {
-    return data.json()
-  })
-  .then(displayResults)
+function getLocationCoordinates(event) {
+    if (event.keyCode == 13) {
+        getResults(inputCity.value)
+    } else {
+        getResults(inputCity.value)
+    }
 }
 
-function displayResults (data) {
-  lat = data[0].lat
-  lon = data[0].lon
-  const urlCoordinate = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+function getResults(query) {
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&units=metric&appid=${API_KEY}`)
+        .then((data) => {
+            return data.json()
+        })
+        .then(displayResults)
+}
+
+function displayResults(data) {
+ /*   lat = data[0].lat
+    lon = data[0].lon*/
+    const urlCoordinate = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
 
     fetch(urlCoordinate)
         .then((response) => {
@@ -85,18 +80,9 @@ function displayResults (data) {
         })
 }
 
-async function reverseLocation(lat, lon) {
-    let url = fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`)
-        .then(response => response.json())
-        .then (data => {
-            temp.innerHTML = (data.main.temp).toFixed(0) + '°c'
-            description.innerHTML = data.weather[0]['description']
-        })
-}
-
 function day() {
     let day = new Date();
-    let weekDay = [
+    let weekDays = [
         "Sunday",
         "Monday",
         "Tuesday",
@@ -104,51 +90,69 @@ function day() {
         "Thursday",
         "Friday",
         "Saturday"
-    ];
+    ]
+
     for (let i = 0; i < 5; i++) {
-        const getDays = weekDay[day.getDay()]
-        console.log(weekDay[(day.getDay() + 1 + i) % 7]);
+        let dayOfTheWeek = weekDays[(day.getDay() + i) % 7]
     }
     console.log(day)
 }
 
 let forecast = `https://api.openweathermap.org/data/2.5/forecast?q=gent&cnt=5&units=metric&appid=${API_KEY}`
 
-fetch(forecast)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-        data.list.forEach(list => {
+inputCity.addEventListener('keyup', (event) => {
+    if (event.key === "Enter") {
+        const thisCity = event.currentTarget.value.toLowerCase();
+        const apiUrl = "https://api.openweathermap.org/data/2.5/forecast/?q=" + thisCity + "&appid=" + API_KEY
+        event.currentTarget.value = ''
 
-            const container = document.querySelector('.five-days')
+        fetch(forecast)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                console.log(data)
+                data.list.forEach(list => {
 
-            const forecastCard = document.createElement('div')
-            container.appendChild(forecastCard)
-            forecastCard.classList.add('forecast-card')
+                    const container = document.querySelector('.five-days')
 
-/*            const date = document.createElement('h2')
-            date.textContent = day
-            forecastCard.appendChild(date)
-            date.classList.add('date')*/
+                    const forecastCard = document.createElement('div')
+                    container.appendChild(forecastCard)
+                    forecastCard.classList.add('forecast-card')
 
-            const currentTemp = document.createElement('p')
-            currentTemp.textContent = (list.main.temp).toFixed(0) + '°C'
-            forecastCard.appendChild(currentTemp)
-            currentTemp.classList.add('current-temp')
+                    const date = document.createElement('h2')
+                    date.textContent = dayOfTheWeek
+                    forecastCard.appendChild(date)
+                    date.classList.add('date')
 
-            const conditions = document.createElement('p')
-            conditions.textContent = list.weather[0]['main']
-            forecastCard.appendChild(conditions)
-            conditions.classList.add('conditions')
+                    const currentTemp = document.createElement('p')
+                    currentTemp.textContent = (list.main.temp).toFixed(0) + '°C'
+                    forecastCard.appendChild(currentTemp)
+                    currentTemp.classList.add('current-temp')
 
-            const icon = document.createElement('img')
-            icon.src = `http://openweathermap.org/img/w/${list.weather[0].icon}.png`
-            forecastCard.appendChild(icon)
-            icon.classList.add('icon')
-            day()
+                    const conditions = document.createElement('p')
+                    conditions.textContent = list.weather[0]['main']
+                    forecastCard.appendChild(conditions)
+                    conditions.classList.add('conditions')
+
+                    const icon = document.createElement('img')
+                    icon.src = `http://openweathermap.org/img/w/${list.weather[0].icon}.png`
+                    forecastCard.appendChild(icon)
+                    icon.classList.add('icon')
+                    day()
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+})
+
+async function reverseLocation(lat, lon) {
+    let url = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            temp.innerHTML = (data.main.temp).toFixed(0) + '°c'
+            description.innerHTML = data.weather[0]['description']
         })
-    })
-.catch(error => {
-    console.log(error)})
+}
